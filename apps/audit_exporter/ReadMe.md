e47160af1eeca190e4c9aec71d76d967a8b07928
orlov89 <orlov89@gmail.com>
2022-09-14 12:23:47 +0300

    audit export fixes

--- apps/audit_exporter/ReadMe.md
+++ apps/audit_exporter/ReadMe.md
@@ -3,10 +3,10 @@
 **core script** - base.sh. Allow to export audit from middlework or login center with a provided date range. 
 Script creates files with export per 1 day based on server date in a provided folder.
 use -h or --help to read about all input attributes.
-example:
+examples:
 
 ./base.sh -t "lc" -p "/om/optimacros/login-center/.env" -s "2022-09-08" -e "2022-09-09" -o "/mnt/export/some/folder"
-
+./base.sh -t "mw" -p "/path/to/manifest.json" -v "/path/to/vagrant/container" -s "2022-08-01" -e "2022-09-09" -o "/some/output/dir"
 
 **used env variables** - audit_export.sh
 used to simplify export with saving input to .env file
--- apps/audit_exporter/base.sh
+++ apps/audit_exporter/base.sh
@@ -10,13 +10,16 @@ while [[ $# -gt 0 ]]; do
       echo "Audit [options] application [arguments]"
       echo " "
       echo "options:"
-      echo "-h, --help        show help"
-      echo "-t, --type        application type. Possible values: mw - middlework application; lc - login center"
-      echo "-c, --config      config path. Path to manifest in case of MW or to .env file in case of LC contained password"
-      echo "-p, --path        project path. Path to project exported from"
-      echo "-s, --startDate   export starting from UTC date(Y-m-d format)"
-      echo "-e, --endDate     export up to from UTC date(Y-m-d format). End date must be greater than start date"
-      echo "-o, --output      output directory"
+      echo "-h, --help         show help"
+      echo "-t, --type         application type. Possible values: mw - middlework application; lc - login center"
+      echo "-p, --pathConfig   config path. Path to manifest in case of MW or to .env file in case of LC contained password"
+      echo "-v, --vagrant      path to vagrant container file exported from"
+      echo "-s, --startDate    export starting from UTC date(Y-m-d format)"
+      echo "-e, --endDate      export up to from UTC date(Y-m-d format). End date must be greater than start date"
+      echo "-o, --output       output directory"
+      echo "-u, --user         mongo user"
+      echo "-d, --db           mongo database"
+      echo "-c, --collection   mongo collection"
       exit 0
       ;;
     -t|--type)
@@ -24,13 +27,13 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
-    -c|--config)
+    -p|--pathConfig)
       configPath="$2"
       shift
       shift
       ;;
-    -p|--path)
-      projectPath="$2"
+    -v|--vagrantPath)
+      vagrantPath="$2"
       shift
       shift
       ;;
@@ -49,6 +52,21 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+    -u|--user)
+      mUser="$2"
+      shift
+      shift
+      ;;
+    -d|--db)
+      mDB="$2"
+      shift
+      shift
+      ;;
+    -c|--collection)
+      mCollection="$2"
+      shift
+      shift
+      ;;
     -*|--*)
       echo "Unknown option $1"
       exit 1
@@ -63,10 +81,101 @@ done
 tmp="/tmp/temp_audit_file"
 d="$startDate"
 
+if [ -z "$startDate" ]
+then
+  echo "start date flag is empty, terminated"
+  exit 1
+fi
+
+if [ -z "$endDate" ]
+then
+  echo "end date flag is empty, terminated"
+  exit 1
+fi
+
+if [ -z "$configPath" ]
+then
+  echo "end date flag is empty, terminated"
+  exit 1
+fi
+
+if [ -z "$outputDir" ]
+then
+  echo "target dir flag is empty, terminated"
+  exit 1
+fi
+
+if [[ "$appType" == "mw" ]]; then
+    if [ -z "$vagrantPath" ]
+    then
+      echo "path to directory with vagrant container is empty, terminated"
+      exit 1
+    fi
+
+    if [ -z "$mUser" ]
+    then
+      mUser='admin'
+    fi
+
+    mPassword=$(cat "$configPath" | grep "mongodb" -C3 | grep "$mUser" | awk -v RS='\r\n' "/$mUser/ { gsub(/[\",]/,\"\",\$2); print \$2}")
+
+    if [ -z "$vagrantPath" ]
+    then
+      echo "Can not parse password for user: $mUser"
+      exit 1
+    fi
+
+    if [ -z "$mDB" ]
+    then
+      mDB='optimacros_audit'
+    fi
+
+    if [ -z "$mCollection" ]
+    then
+      mCollection='events'
+    fi
+
+    version=$(cd "$vagrantPath" && vagrant ssh -- -t -q cat /home/vagrant/optimacros_middlework/app/config/version.php | grep return | cut -d"'" -f 2)
+
+  elif [[ "$appType" == "lc" ]]; then
+    if [ -z "$mUser" ]
+    then
+      mUser=$(cat "$configPath" | grep 'DB_USERNAME' | awk -v RS='\r\n' -F'=' '{print $2}')
+    fi
+
+    mPassword=$(cat "$configPath" | grep 'DB_PASSWORD' | awk -v RS='\r\n' -F'=' '{print $2}')
+
+    if [ -z "$mPassword" ]
+    then
+      echo "Can not parse password for user: $mUser"
+      exit 1
+    fi
+
+    if [ -z "$mDB" ]
+    then
+      mDB='logincenter'
+    fi
+
+    if [ -z "$mCollection" ]
+    then
+      mCollection='audit'
+    fi
+
+    version=$(cat "$configPath" | grep 'VERSION' | awk -v RS='\r\n' -F'=' '{print $2}')
+  else
+    echo "wrong application type provided"
+    exit 1
+  fi
+
+
+endDate=$(date -I -d "$endDate + 1 day")
+
 while [ "$d" != "$endDate" ]; do
   echo "Start export for date: $d"
 
   dateF=$(date -d "$d" +'%F')
+  dateNowDate=$(date +"%F")
+  dateNowTime=$(date +"%T")
   dateTsStart=$(date -d "$d 00:00:00" +'%s')
   dateTsEnd=$(date -d "$d 23:59:59" +'%s')
   auditFileName="$dateF"'_om_'"$appType"'_audit_log.txt'
@@ -81,22 +190,14 @@ while [ "$d" != "$endDate" ]; do
 
   touch "$tmp"
 
+  echo "{\"version\":\"$version\", \"type\":\"$appType\", \"datetime\":\"$dateNowDate $dateNowTime\"}" >> "$FILE"
+
   if [[ "$appType" == "mw" ]]; then
-    mUser='admin'
-    mPassword=$(cd "$projectPath" && vagrant ssh -- -t -q cat "$configPath" | grep "mongodb" -C3 | grep "$mUser" | awk -v RS='\r\n' "/$mUser/ { gsub(/[\",]/,\"\",\$2); print \$2}")
-    mDB='optimacros_audit'
-    mCollection='events'
-    version=$(cd "$projectPath" && vagrant ssh -- -t -q cat /home/vagrant/optimacros_middlework/app/config/version.php | grep return | cut -d"'" -f 2)
-    echo "{\"version\":\"$version\", \"type\":\"$appType\", \"datetime\":\"$dateF $dateT\"}" >> "$FILE"
-    $(cd "$projectPath" && vagrant ssh -- -t -q mongoexport -d="$mDB" -c="$mCollection" -u="$mUser" -p="$mPassword" -q=\'{\"createAt\": { \"\$gte\" : "$dateTsStart", \"\$lt\" : "$dateTsEnd" } }\' --sort=\'{id: -1}\' --authenticationDatabase=admin >> "$tmp")
+#    echo "cd \"$vagrantPath\" && vagrant ssh -- -t -q mongoexport -d=\"$mDB\" -c=\"$mCollection\" -u=\"$mUser\" -p=\"$mPassword\" -q=\'{\"createAt\": { \"\$gte\" : \"$dateTsStart\", \"\$lt\" : \"$dateTsEnd\" } }\' --sort=\'{id: -1}\' --authenticationDatabase=admin"
+    $(cd "$vagrantPath" && vagrant ssh -- -t -q mongoexport -d="$mDB" -c="$mCollection" -u="$mUser" -p="$mPassword" -q=\'{\"createAt\": { \"\$gte\" : "$dateTsStart", \"\$lt\" : "$dateTsEnd" } }\' --sort=\'{id: -1}\' --authenticationDatabase=admin >> "$tmp")
   elif [[ "$appType" == "lc" ]]; then
-    mUser='login-center'
-    mPassword=$(cat "$configPath" | grep 'DB_PASSWORD' | awk -v RS='\r\n' -F'=' '{print $2}')
-    mDB='logincenter'
-    mCollection='audit'
-    version=$(cat "$configPath" | grep 'VERSION' | awk -v RS='\r\n' -F'=' '{print $2}')
-    echo "{\"version\":\"$version\", \"type\":\"$appType\", \"datetime\":\"$dateF $dateT\"}" >> "$FILE"
-    $(docker exec -ti optimacros_db mongoexport -d="$mDB" -c="$mCollection" -u="$mUser" -p="$mPassword" -q='{"createAt": { "$gte" : "$dateTsStart", "$lt" : "$dateTsEnd" } }' --sort='{id: -1}' --authenticationDatabase=admin >> "$tmp")
+#    echo "docker exec -ti optimacros_db mongoexport -d=\"$mDB\" -c=\"$mCollection\" -u=\"$mUser\" -p=\"$mPassword\" -q='{\"createAt\": { \"\$gte\" : \"$dateTsStart\", \"\$lt\" : \"$dateTsEnd\" } }' --sort='{id: -1}' --authenticationDatabase=admin"
+    $(docker exec -ti optimacros_db mongoexport -d="$mDB" -c="$mCollection" -u="$mUser" -p="$mPassword" -q='{"createAt": { "\$gte" : "$dateTsStart", "\$lt" : "$dateTsEnd" } }' --sort='{id: -1}' --authenticationDatabase=admin >> "$tmp")
   else
     echo "wrong application type provided"
     exit 1
